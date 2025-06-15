6e124c0e04083f2d1cd7c49db9920d0fb9c468e3
Ali Baran Demir <alibarandemir798@gmail.com>
2025-06-15 19:39:19 +0300

    appointment dto updated

--- src/main/java/com/clinic/hospital_appointment_backend/service/PatientService.java
+++ src/main/java/com/clinic/hospital_appointment_backend/service/PatientService.java
@@ -1,6 +1,6 @@
 package com.clinic.hospital_appointment_backend.service;
 
-import com.clinic.hospital_appointment_backend.dto.CreateAppointmentRequest;
+
 import com.clinic.hospital_appointment_backend.dto.PatientResponseDto;
 import com.clinic.hospital_appointment_backend.dto.ResponseDto;
 import org.springframework.security.core.Authentication;
